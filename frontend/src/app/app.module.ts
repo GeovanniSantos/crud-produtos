48ea4875a8f000026e9ba90c95671f1578d4df19
Geovanni Santos <gfc.net@gmail.com>
2022-03-27 15:16:25 -0300

    Excluir produto

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -32,6 +32,8 @@ import { MatSortModule } from '@angular/material/sort'
 
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 
 registerLocaleData(localePt);
 
@@ -46,7 +48,9 @@ registerLocaleData(localePt);
     RedDirective,
     ProductCreateComponent,
     ProductReadComponent,
-    ProductRead2Component
+    ProductRead2Component,
+    ProductUpdateComponent,
+    ProductDeleteComponent
   ],
   imports: [
     BrowserModule,
